7bdef8d0797c40cbe828893974851211a91ad904
Tachi <zaveruha007@gmail.com>
2015-06-23 13:53:35 +0300

    Initial Deploy

--- index.php
+++ index.php
@@ -12,19 +12,39 @@
 				<h1>Tic Tac Toe</h1>
 			</header>
 			<ul id="board">
-				<li id="spot1">+</li>
-				<li id="spot2">+</li>
-				<li id="spot3">+</li>
-				<li id="spot4">+</li>
-				<li id="spot5">+</li>
-				<li id="spot6">+</li>
-				<li id="spot7">+</li>
-				<li id="spot8">+</li>
-				<li id="spot9">+</li>
+				<li id="spot1">
+					+
+				</li>
+				<li id="spot2">
+					+
+				</li>
+				<li id="spot3">
+					+
+				</li>
+				<li id="spot4">
+					+
+				</li>
+				<li id="spot5">
+					+
+				</li>
+				<li id="spot6">
+					+
+				</li>
+				<li id="spot7">
+					+
+				</li>
+				<li id="spot8">
+					+
+				</li>
+				<li id="spot9">
+					+
+				</li>
 			</ul>
 			<div class="clearfix"></div>
 			<footer>
-				<button id="reset">Reset Game</button>
+				<button id="reset">
+					Reset Game
+				</button>
 			</footer>
 		</div>
 	</body>
